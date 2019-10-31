83d936abde3e0629153123407794629eeba905ad
Abdul Rauf <raufyy4@gmail.com>
2019-10-31 09:37:12 +0500

    deleted everything

--- searchpage.js
+++
@@ -1,2 +0,0 @@
-let intro = "<br><div id='results' class='card m-card'><h4 class=''>Search results for </h4></div>";
-let searchbox= '<br><div class="welcome story labs" style="background-color: rgba(255, 255, 255 ,0.7 ); "id= "searchbox"></div>'
